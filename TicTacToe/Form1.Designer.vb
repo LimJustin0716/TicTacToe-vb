32d1c92e738d87c130abdd00ef228181942d24a1
justinlim16 <justinlim.dn98@gmail.com>
2022-06-30 02:25:54 +0800

    Added result checker functionality and UI design

--- TicTacToe/Form1.Designer.vb
+++ TicTacToe/Form1.Designer.vb
@@ -37,108 +37,162 @@ Partial Class Form1
         '
         'top_left
         '
+        Me.top_left.BackColor = System.Drawing.Color.Black
+        Me.top_left.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.top_left.FlatAppearance.BorderSize = 2
+        Me.top_left.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.top_left.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.top_left.ForeColor = System.Drawing.Color.Lime
         Me.top_left.Location = New System.Drawing.Point(12, 12)
         Me.top_left.Name = "top_left"
         Me.top_left.Size = New System.Drawing.Size(176, 180)
         Me.top_left.TabIndex = 0
-        Me.top_left.UseVisualStyleBackColor = True
+        Me.top_left.UseVisualStyleBackColor = False
         '
         'top_mid
         '
+        Me.top_mid.BackColor = System.Drawing.Color.Black
+        Me.top_mid.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.top_mid.FlatAppearance.BorderSize = 2
+        Me.top_mid.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.top_mid.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.top_mid.ForeColor = System.Drawing.Color.Lime
         Me.top_mid.Location = New System.Drawing.Point(194, 12)
         Me.top_mid.Name = "top_mid"
         Me.top_mid.Size = New System.Drawing.Size(176, 180)
         Me.top_mid.TabIndex = 1
-        Me.top_mid.UseVisualStyleBackColor = True
+        Me.top_mid.UseVisualStyleBackColor = False
         '
         'top_right
         '
+        Me.top_right.BackColor = System.Drawing.Color.Black
+        Me.top_right.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.top_right.FlatAppearance.BorderSize = 2
+        Me.top_right.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.top_right.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.top_right.ForeColor = System.Drawing.Color.Lime
         Me.top_right.Location = New System.Drawing.Point(376, 12)
         Me.top_right.Name = "top_right"
         Me.top_right.Size = New System.Drawing.Size(176, 180)
         Me.top_right.TabIndex = 2
-        Me.top_right.UseVisualStyleBackColor = True
+        Me.top_right.UseVisualStyleBackColor = False
         '
         'mid_left
         '
         Me.mid_left.AccessibleDescription = ""
+        Me.mid_left.BackColor = System.Drawing.Color.Black
+        Me.mid_left.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.mid_left.FlatAppearance.BorderSize = 2
+        Me.mid_left.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.mid_left.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.mid_left.ForeColor = System.Drawing.Color.Lime
         Me.mid_left.Location = New System.Drawing.Point(12, 198)
         Me.mid_left.Name = "mid_left"
         Me.mid_left.Size = New System.Drawing.Size(176, 180)
         Me.mid_left.TabIndex = 3
-        Me.mid_left.UseVisualStyleBackColor = True
+        Me.mid_left.UseVisualStyleBackColor = False
         '
         'mid
         '
+        Me.mid.BackColor = System.Drawing.Color.Black
+        Me.mid.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.mid.FlatAppearance.BorderSize = 2
+        Me.mid.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.mid.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.mid.ForeColor = System.Drawing.Color.Lime
         Me.mid.Location = New System.Drawing.Point(194, 198)
         Me.mid.Name = "mid"
         Me.mid.Size = New System.Drawing.Size(176, 180)
         Me.mid.TabIndex = 4
-        Me.mid.UseVisualStyleBackColor = True
+        Me.mid.UseVisualStyleBackColor = False
         '
         'mid_right
         '
+        Me.mid_right.BackColor = System.Drawing.Color.Black
+        Me.mid_right.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.mid_right.FlatAppearance.BorderSize = 2
+        Me.mid_right.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.mid_right.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.mid_right.ForeColor = System.Drawing.Color.Lime
         Me.mid_right.Location = New System.Drawing.Point(376, 198)
         Me.mid_right.Name = "mid_right"
         Me.mid_right.Size = New System.Drawing.Size(176, 180)
         Me.mid_right.TabIndex = 5
-        Me.mid_right.UseVisualStyleBackColor = True
+        Me.mid_right.UseVisualStyleBackColor = False
         '
         'bot_left
         '
+        Me.bot_left.BackColor = System.Drawing.Color.Black
+        Me.bot_left.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.bot_left.FlatAppearance.BorderSize = 2
+        Me.bot_left.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.bot_left.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bot_left.ForeColor = System.Drawing.Color.Lime
         Me.bot_left.Location = New System.Drawing.Point(12, 384)
         Me.bot_left.Name = "bot_left"
         Me.bot_left.Size = New System.Drawing.Size(176, 180)
         Me.bot_left.TabIndex = 6
-        Me.bot_left.UseVisualStyleBackColor = True
+        Me.bot_left.UseVisualStyleBackColor = False
         '
         'bot_mid
         '
+        Me.bot_mid.BackColor = System.Drawing.Color.Black
+        Me.bot_mid.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.bot_mid.FlatAppearance.BorderSize = 2
+        Me.bot_mid.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.bot_mid.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bot_mid.ForeColor = System.Drawing.Color.Lime
         Me.bot_mid.Location = New System.Drawing.Point(194, 384)
         Me.bot_mid.Name = "bot_mid"
         Me.bot_mid.Size = New System.Drawing.Size(176, 180)
         Me.bot_mid.TabIndex = 7
-        Me.bot_mid.UseVisualStyleBackColor = True
+        Me.bot_mid.UseVisualStyleBackColor = False
         '
         'bot_right
         '
+        Me.bot_right.BackColor = System.Drawing.Color.Black
+        Me.bot_right.FlatAppearance.BorderColor = System.Drawing.Color.Lime
+        Me.bot_right.FlatAppearance.BorderSize = 2
+        Me.bot_right.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.bot_right.Font = New System.Drawing.Font("Segoe UI", 80.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.bot_right.ForeColor = System.Drawing.Color.Lime
         Me.bot_right.Location = New System.Drawing.Point(376, 384)
         Me.bot_right.Name = "bot_right"
         Me.bot_right.Size = New System.Drawing.Size(176, 180)
         Me.bot_right.TabIndex = 8
-        Me.bot_right.UseVisualStyleBackColor = True
+        Me.bot_right.UseVisualStyleBackColor = False
         '
         'restart_btn
         '
+        Me.restart_btn.BackColor = System.Drawing.Color.Black
+        Me.restart_btn.FlatAppearance.BorderColor = System.Drawing.Color.Red
+        Me.restart_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.restart_btn.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.restart_btn.ForeColor = System.Drawing.Color.Red
         Me.restart_btn.Location = New System.Drawing.Point(577, 464)
         Me.restart_btn.Name = "restart_btn"
         Me.restart_btn.Size = New System.Drawing.Size(165, 61)
         Me.restart_btn.TabIndex = 9
         Me.restart_btn.Text = "Restart"
-        Me.restart_btn.UseVisualStyleBackColor = True
+        Me.restart_btn.UseVisualStyleBackColor = False
         '
         'turn_disp
         '
         Me.turn_disp.AutoSize = True
-        Me.turn_disp.Location = New System.Drawing.Point(638, 69)
+        Me.turn_disp.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.turn_disp.ForeColor = System.Drawing.Color.Lime
+        Me.turn_disp.Location = New System.Drawing.Point(577, 67)
         Me.turn_disp.Name = "turn_disp"
-        Me.turn_disp.Size = New System.Drawing.Size(0, 20)
+        Me.turn_disp.Size = New System.Drawing.Size(146, 46)
         Me.turn_disp.TabIndex = 10
+        Me.turn_disp.Text = "Player: X"
         Me.turn_disp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(754, 581)
         Me.Controls.Add(Me.turn_disp)
         Me.Controls.Add(Me.restart_btn)
--- TicTacToe/Form1.vb
+++ TicTacToe/Form1.vb
@@ -31,6 +31,8 @@
     Public Function PrintButtonText(ByVal btn As Button)
         If btn.Text = "" Then
             btn.Text = Player
+            result_checker()
+
             disp_turn()
         End If
         Return True
@@ -71,4 +73,31 @@
     Private Sub bot_right_Click(sender As Object, e As EventArgs) Handles bot_right.Click
         PrintButtonText(bot_right)
     End Sub
+
+    Public Function result_checker()
+        If top_left.Text = top_mid.Text And top_mid.Text = top_right.Text Then
+            win(top_left, top_mid, top_right)
+        ElseIf top_left.Text = mid.Text And mid.Text = bot_right.Text Then
+            win(top_left, mid, bot_right)
+        ElseIf mid_left.Text = mid.Text And mid.Text = mid_right.Text Then
+            win(mid_left, mid, mid_right)
+        ElseIf bot_left.Text = bot_mid.Text And bot_mid.Text = bot_right.Text Then
+            win(bot_left, bot_mid, bot_right)
+        ElseIf top_left.Text = mid_left.Text And mid_left.Text = bot_left.Text Then
+            win(top_left, mid_left, bot_left)
+        ElseIf top_mid.Text = mid.Text And mid.Text = bot_mid.Text Then
+            win(top_mid, mid, bot_mid)
+        ElseIf top_right.Text = mid_right.Text And mid_right.Text = bot_right.Text Then
+            win(top_right, mid_right, bot_right)
+        ElseIf top_right.Text = mid.Text And mid.Text = bot_left.Text Then
+            win(top_right, mid, bot_left)
+        End If
+    End Function
+    Public Function win(ByVal Button1 As Button, ByVal Button2 As Button, ByVal Button3 As Button)
+        If Button1.Text = "" Or Button2.Text = "" Or Button3.Text = "" Then
+        Else
+            MessageBox.Show(Button1.Text.ToString + " is the Winner")
+        End If
+
+    End Function
 End Class
